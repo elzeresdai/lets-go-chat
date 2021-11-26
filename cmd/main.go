ae848e825b192c5ac97359d9b1c9603da010521f
Tonya <elzeresdai@gmail.com>
2021-11-26 14:08:09 +0200

    added env

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"flag"
 	"lets-go-chat/internal/server"
+	"os"
 )
+
+type options struct {
+	port string
+}
+
+var opt options
+
+func init() {
+	flag.StringVar(&opt.port, "p", os.Getenv("PORT"), "The default port to listen on")
+	flag.Parse()
+
+	if opt.port == "" {
+		opt.port = "7"
+	}
+}
 
 func main() {
 	e := server.New()
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,15 +3,24 @@ package server
 import (
 	"github.com/labstack/echo/v4"
 	"lets-go-chat/internal/handlers"
+	"log"
 	"net/http"
+	"os"
 )
 
 func New() *echo.Echo {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	e := echo.New()
 	e.GET("/", hello)
 	e.POST("/users", handlers.HandleUserCreate)
 	e.POST("/login", handlers.HandleUserLogin)
-	e.Logger.Fatal(e.Start(":8000"))
+	e.Logger.Fatal(e.Start(":" + port))
 	return e
 }
 
